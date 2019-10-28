124f1abcaa83f396483eee4458538b59ec9fdc72
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2019-10-28 16:31:04 -0700

    Update setup.py

--- mario/data/setup.py
+++ mario/data/setup.py
@@ -1,14 +1,10 @@
-"""
-This module initializes the display and creates dictionaries of resources.
-"""
-
+#initializes display and creates dictionaries of resources
 import os
 import pygame as pg
 from . import tools
 from .import constants as c
 
 ORIGINAL_CAPTION = c.ORIGINAL_CAPTION
-
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 pg.init()
 pg.event.set_allowed([pg.KEYDOWN, pg.KEYUP, pg.QUIT])
@@ -20,5 +16,3 @@ FONTS = tools.load_all_fonts(os.path.join("resources","fonts"))
 MUSIC = tools.load_all_music(os.path.join("resources","music"))
 GFX   = tools.load_all_gfx(os.path.join("resources","graphics"))
 SFX   = tools.load_all_sfx(os.path.join("resources","sound"))
-
-
