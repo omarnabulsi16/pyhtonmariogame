7e11402d63a06058396dc3ac678dfb515d3d0755
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2019-10-28 18:52:41 -0700

    Update checkpoint.py

--- mario/data/components/checkpoint.py
+++ mario/data/components/checkpoint.py
@@ -1,10 +1,8 @@
 import pygame as pg
 from .. import constants as c
 
-
 class Checkpoint(pg.sprite.Sprite):
-    """Invisible sprite used to add enemies, special boxes
-    and trigger sliding down the flag pole"""
+    #Invisible sprite used to add enemies, special boxes and trigger sliding down the flag pole
     def __init__(self, x, name, y=0, width=10, height=600):
         super(Checkpoint, self).__init__()
         self.image = pg.Surface((width, height))
@@ -13,7 +11,3 @@ class Checkpoint(pg.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
         self.name = name
-
-
-
-
