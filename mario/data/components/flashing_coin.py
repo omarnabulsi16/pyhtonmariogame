27b11c994cb1a9c87b58f25d576c1823bf870826
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2019-10-28 19:14:10 -0700

    Update flashing_coin.py

--- mario/data/components/flashing_coin.py
+++ mario/data/components/flashing_coin.py
@@ -3,7 +3,7 @@ from .. import setup
 from .. import constants as c
 
 class Coin(pg.sprite.Sprite):
-    """Flashing coin next to coin total info"""
+    #flashing coin next to coin total info
     def __init__(self, x, y):
         super(Coin, self).__init__()
         self.sprite_sheet = setup.GFX['item_objects']
@@ -16,32 +16,25 @@ class Coin(pg.sprite.Sprite):
         self.first_half = True
         self.frame_index = 0
 
-
     def create_frames(self):
-        """Extract coin images from sprite sheet and assign them to a list"""
+        #get coin images from sprite sheet and assign them to a list
         self.frames = []
         self.frame_index = 0
-
-        self.frames.append(self.get_image(1, 160, 5, 8))
-        self.frames.append(self.get_image(9, 160, 5, 8))
-        self.frames.append(self.get_image(17, 160, 5, 8))
-
+        self.frames.append(self.get_image(1,160,5,8))
+        self.frames.append(self.get_image(9,160,5,8))
+        self.frames.append(self.get_image(17,160,5,8))
 
     def get_image(self, x, y, width, height):
-        """Extracts image from sprite sheet"""
+        #get image from sprite 
         image = pg.Surface([width, height])
         rect = image.get_rect()
-
         image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
         image.set_colorkey(c.BLACK)
-        image = pg.transform.scale(image,
-                                   (int(rect.width*c.BRICK_SIZE_MULTIPLIER),
-                                    int(rect.height*c.BRICK_SIZE_MULTIPLIER)))
+        image = pg.transform.scale(image,(int(rect.width*c.BRICK_SIZE_MULTIPLIER),int(rect.height*c.BRICK_SIZE_MULTIPLIER)))
         return image
 
-
     def update(self, current_time):
-        """Animates flashing coin"""
+        #animates flashing coin
         if self.first_half:
             if self.frame_index == 0:
                 if (current_time - self.timer) > 375:
@@ -62,5 +55,4 @@ class Coin(pg.sprite.Sprite):
                     self.frame_index -= 1
                     self.first_half = True
                     self.timer = current_time
-
         self.image = self.frames[self.frame_index]
