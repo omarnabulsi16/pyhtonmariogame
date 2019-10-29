f59e7d88b6efc411ceb312ef734f635eb733fd48
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2019-10-28 19:59:38 -0700

    Update info.py

--- mario/data/components/info.py
+++ mario/data/components/info.py
@@ -173,7 +173,6 @@ class OverheadInfo(object):
         top = []
         top_score = []
         self.create_label(player_one_game, '1 PLAYER GAME', 272, 360)
-        self.create_label(player_two_game, '2 PLAYER GAME', 272, 405)
         self.create_label(top, 'TOP - ', 290, 465)
         self.create_label(top_score, '000000', 400, 465)
         self.main_menu_labels = [player_one_game, player_two_game,top, top_score]
