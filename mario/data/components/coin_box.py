a42695aff8418a5cc0f32447ef1d072470f146c5
omarnabulsi16 <31221375+omarnabulsi16@users.noreply.github.com>
2019-10-28 18:57:32 -0700

    Update coin_box.py

--- mario/data/components/coin_box.py
+++ mario/data/components/coin_box.py
@@ -5,7 +5,7 @@ from . import powerups
 from . import coin
 
 class Coin_box(pg.sprite.Sprite):
-    """Coin box sprite"""
+    #Coin box sprite
     def __init__(self, x, y, contents='coin', group=None):
         pg.sprite.Sprite.__init__(self)
         self.sprite_sheet = setup.GFX['tile_set']
@@ -26,23 +26,17 @@ class Coin_box(pg.sprite.Sprite):
         self.contents = contents
         self.group = group
 
-
     def get_image(self, x, y, width, height):
-        """Extract image from sprite sheet"""
+        #Extract image from sprite sheet
         image = pg.Surface([width, height]).convert()
         rect = image.get_rect()
-
         image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
         image.set_colorkey(c.BLACK)
-
-        image = pg.transform.scale(image,
-                                   (int(rect.width*c.BRICK_SIZE_MULTIPLIER),
-                                    int(rect.height*c.BRICK_SIZE_MULTIPLIER)))
+        image = pg.transform.scale(image,(int(rect.width*c.BRICK_SIZE_MULTIPLIER),int(rect.height*c.BRICK_SIZE_MULTIPLIER)))
         return image
 
-
     def setup_frames(self):
-        """Create frame list"""
+        #Create frame list
         self.frames.append(
             self.get_image(384, 0, 16, 16))
         self.frames.append(
@@ -52,16 +46,13 @@ class Coin_box(pg.sprite.Sprite):
         self.frames.append(
             self.get_image(432, 0, 16, 16))
 
-
     def update(self, game_info):
-        """Update coin box behavior"""
+        #update coin box behavior
         self.current_time = game_info[c.CURRENT_TIME]
         self.handle_states()
 
-
     def handle_states(self):
-        """Determine action based on RESTING, BUMPED or OPENED
-        state"""
+        #determine action based on RESTING, BUMPED or OPENED state
         if self.state == c.RESTING:
             self.resting()
         elif self.state == c.BUMPED:
@@ -69,9 +60,8 @@ class Coin_box(pg.sprite.Sprite):
         elif self.state == c.OPENED:
             self.opened()
 
-
     def resting(self):
-        """Action when in the RESTING state"""
+        #action when in the RESTING state
         if self.first_half:
             if self.frame_index == 0:
                 if (self.current_time - self.animation_timer) > 375:
@@ -92,15 +82,12 @@ class Coin_box(pg.sprite.Sprite):
                     self.frame_index -= 1
                     self.first_half = True
                     self.animation_timer = self.current_time
-
         self.image = self.frames[self.frame_index]
 
-
     def bumped(self):
-        """Action after Mario has bumped the box from below"""
+        #action after Mario has bumped the box from below
         self.rect.y += self.y_vel
         self.y_vel += self.gravity
-
         if self.rect.y > self.rest_height + 5:
             self.rect.y = self.rest_height
             self.state = c.OPENED
@@ -110,37 +97,18 @@ class Coin_box(pg.sprite.Sprite):
                 self.group.add(powerups.FireFlower(self.rect.centerx, self.rect.y))
             elif self.contents == '1up_mushroom':
                 self.group.add(powerups.LifeMushroom(self.rect.centerx, self.rect.y))
-
-
         self.frame_index = 3
         self.image = self.frames[self.frame_index]
 
-
     def start_bump(self, score_group):
-        """Transitions box into BUMPED state"""
+        #transitions box into BUMPED state
         self.y_vel = -6
         self.state = c.BUMPED
-
         if self.contents == 'coin':
-            self.group.add(coin.Coin(self.rect.centerx,
-                                     self.rect.y,
-                                     score_group))
+            self.group.add(coin.Coin(self.rect.centerx,self.rect.y,score_group))
             setup.SFX['coin'].play()
         else:
             setup.SFX['powerup_appears'].play()
 
-
     def opened(self):
-        """Placeholder for OPENED state"""
         pass
-
-
-
-
-
-
-
-
-
-
-
